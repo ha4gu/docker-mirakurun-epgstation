d1224241b2a96247b2d900e70f3f82380d65009b
Yuta Hashiguchi <9391978+ha4gu@users.noreply.github.com>
2025-11-24 13:13:19 +0900

    epgstationの設定ファイルを更新

--- setup.sh
+++ setup.sh
@@ -3,11 +3,7 @@
 git clone https://github.com/ha4gu/docker-mirakurun-epgstation.git ./tv
 cd ./tv/
 
-cp epgstation/config/enc.js.template epgstation/config/enc.js
-cp epgstation/config/config.yml.template epgstation/config/config.yml
-cp epgstation/config/operatorLogConfig.sample.yml epgstation/config/operatorLogConfig.yml
-cp epgstation/config/epgUpdaterLogConfig.sample.yml epgstation/config/epgUpdaterLogConfig.yml
-cp epgstation/config/serviceLogConfig.sample.yml epgstation/config/serviceLogConfig.yml
+cp epgstation/config/config.sample.yml epgstation/config/config.yml
 
 cp mirakurun/conf/channels.tokyo.yml mirakurun/conf/channels.yml
 cp mirakurun/conf/tuners.sample.yml mirakurun/conf/tuners.yml
